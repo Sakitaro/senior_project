d082687105a4f5f39075c9e528470a9ed7aea9a1
Iwanami Sakitaro <a5819014@aoyama.jp>
2023-11-05 22:29:51 +0900

    エラー処理追加

--- database/database_utils.py
+++ database/database_utils.py
@@ -1,6 +1,7 @@
 import os
 import mysql.connector
 from scraping.extracted_redlinks import process_results
+from mysql.connector import Error
 
 def create_database_connection():
     password = os.getenv('DB_PASSWORD')
@@ -13,30 +14,37 @@ def create_database_connection():
     return cnx
 
 def search_wikipedia(keyword):
-    cnx = create_database_connection()
-    cursor = cnx.cursor(dictionary=True)
+    try:
+        cnx = create_database_connection()
+        cursor = cnx.cursor(dictionary=True)
 
-    # FULLTEXT検索用のSQLクエリに変更
-    sql = """
-    SELECT * FROM text
-    INNER JOIN page ON page.text_id = text.text_id
-    WHERE MATCH(page.page_title) AGAINST(%s IN BOOLEAN MODE);
-    """
+        # FULLTEXT検索用のSQLクエリに変更
+        sql = """
+        SELECT * FROM text
+        INNER JOIN page ON page.text_id = text.text_id
+        WHERE MATCH(page.page_title) AGAINST(%s IN BOOLEAN MODE);
+        """
 
-    # FULLTEXT検索の場合、like_patternは不要
-    # 代わりにキーワードをそのまま使用
-    cursor.execute(sql, (keyword,))
-    result_text = cursor.fetchall()
-    cursor.close()
-    cnx.close()
+        # FULLTEXT検索の場合、like_patternは不要
+        # 代わりにキーワードをそのまま使用
+        cursor.execute(sql, (keyword,))
+        result_text = cursor.fetchall()
+        cursor.close()
+        cnx.close()
 
-    if result_text:
-        print('good')
-        extracted_contents = process_results(result_text)
-        return extracted_contents
-    else:
-        print(f"No results found for '{keyword}'")
-        return []
+        if result_text:
+            print('good')
+            extracted_contents = process_results(result_text)
+            return extracted_contents
+        else:
+            print(f"No results found for '{keyword}'")
+            return []
+    except Error as e:
+        print(f"An error occurred: {e}")
+        # 必要に応じてログファイルに書き込むか、例外を再度投げることもできます。
+        # raise
+        # エラー時の結果は空のリストやNoneで返すことも一般的です。
+        return None
 
 def fetch_page_title():
     cnx = create_database_connection()
